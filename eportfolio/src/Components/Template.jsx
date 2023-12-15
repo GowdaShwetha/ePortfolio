f2043bc2622e91c028b875ae109025e5f60a7b9b
GowdaShwetha <shwethagowda252001@gmail.com>
2023-12-15 14:16:18 +0530

    updated

--- eportfolio/src/Components/Template.jsx
+++ eportfolio/src/Components/Template.jsx
@@ -3,7 +3,7 @@ import  {Link, useNavigate } from 'react-router-dom'
 import "../css/template.css"
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container'
 import * as Icon from "react-bootstrap-icons"
 import Temp1 from '../image/temp1.png'
 import Temp2 from '../image/temp2.png'
@@ -32,18 +32,16 @@ const Template = () => {
     <section id="temp">
       <h1>Templates</h1>
       <Link to='/single'>
-      <Container>
-      <Row id="temps">
+        <Container>
+        <Row id="temps">
         <Col><Link to="/temp01"><img src={Temp1}/></Link></Col>
         <Col><Link to="/temp02"><img src={Temp2}/></Link></Col>
         <Col><Link to="/temp03"><img src={Temp3}/></Link></Col><br/>
       </Row><br/>
-      {/* <Row>
-        <Col><img src="https://i.pinimg.com/564x/90/fe/7c/90fe7c7014869976da794c945372534c.jpg"/></Col>
-        <Col><img src="https://i.pinimg.com/564x/36/49/1b/36491b7667af393c01939fa0e6acc739.jpg"/></Col>
-        <Col><img src="https://i.pinimg.com/564x/a4/ed/5f/a4ed5f04df41b6a82262bd39990fe49b.jpg"/></Col>
-      </Row> */}
-    </Container>
+        </Container>
+     
+      
+  
       </Link>
     </section>
     </>
